4d6ad81504f88d2783ac63e0bdf0f12d6eb8ccb8
Jason McDermott <veloci.cello@gmail.com>
2019-03-17 09:31:10 -0500

    update

--- src/Quiz.js
+++ src/Quiz.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 let quizData = require('./quiz_data.json')
 
-class Quiz extends React.Component {
+class Quiz extends Component {
     constructor(props){
         super(props)
         this.state = { quiz_position: 1 }
@@ -19,4 +19,4 @@ class Quiz extends React.Component {
     }
 }
 
-export default Quiz
+export default Quiz;
